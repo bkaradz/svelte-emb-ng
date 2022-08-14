f17d66fa47c521d824c78fc4cdc016142c9b54dc
bkaradz <karadz@gmail.com>
2022-08-14 22:40:00 +0200

    feat(icons): added extra small X icon

--- src/lib/utility/svgLogos.ts
+++ src/lib/utility/svgLogos.ts
@@ -70,6 +70,9 @@ export const svgWaves = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20
 export const svgXSmall = `<svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <line x1="18" y1="6" x2="6" y2="18" /> <line x1="6" y1="6" x2="18" y2="18" />
 	</svg>`;
 
+export const svgXESmall = `<svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <line x1="18" y1="6" x2="6" y2="18" /> <line x1="6" y1="6" x2="18" y2="18" />
+	</svg>`;
+
 export const svgRefresh = `<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
 	</svg>`;
 
