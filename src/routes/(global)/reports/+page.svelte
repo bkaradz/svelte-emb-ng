834ea43ab4c15ef645c60952fa9af40ec15b7ae4
bkaradz <karadz@gmail.com>
2022-10-10 02:00:29 +0200

    Updated packages and added pdf generation using puppeteer

--- src/routes/(global)/reports/+page.svelte
+++ src/routes/(global)/reports/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import logger from '$lib/utility/logger';
+	import { Buffer } from 'buffer';
 
 	let options: any[];
 	let selected: any;
@@ -21,18 +22,56 @@
 			logger.error(err.message);
 		}
 	};
+
+	// const file = new Blob([response.data], { type: 'application/pdf' })
+
+	//       const fileURL = URL.createObjectURL(file)
+
+	//       const pdfWindow = window.open()
+	//       pdfWindow.location.href = fileURL
+
+	const generatePDF = async () => {
+		const res = await fetch('/api/pdf.json', {
+			method: 'post',
+			headers: {
+				Accept: 'application/json'
+			}
+		});
+
+		if (res.ok) {
+			const json = await res.json();
+			console.log('🚀 ~ file: +page.svelte ~ line 42 ~ generatePDF ~ json', json);
+
+			const pdfBuffer = Buffer.from(json.pdf, 'base64');
+
+			const file = new Blob([pdfBuffer], { type: 'application/pdf' });
+
+			const fileURL = URL.createObjectURL(file);
+
+			const pdfWindow = window.open();
+
+			if (pdfWindow) {
+				pdfWindow.location.href = fileURL;
+			}
+		}
+	};
 </script>
 
 <div>
-	{#if options?.length}
-		<select bind:value={selected}>
-			{#each filterOptionsGroup('embroideryTypes') as item}
-				<option value={item}>
-					{item.name}
-				</option>
-			{/each}
-		</select>
-	{/if}
+	<div>
+		{#if options?.length}
+			<select bind:value={selected}>
+				{#each filterOptionsGroup('embroideryTypes') as item}
+					<option value={item}>
+						{item.name}
+					</option>
+				{/each}
+			</select>
+		{/if}
+	</div>
+	<div class="btn btn-primary mt-4">
+		<button on:click={generatePDF}> Generate PDF </button>
+	</div>
 </div>
 
 <style>
