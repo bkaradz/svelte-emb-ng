f3d3a700fd83b30e52a06b5569fcdc89e8076dbc
karadz <karadz@gmail.com>
2023-06-16 10:04:03 +0200

    WIP lucia

--- src/lib/lucia/client.ts
+++ src/lib/lucia/client.ts
@@ -1,9 +1,21 @@
 import prisma from "$lib/prisma/client";
 import prismaAdapter from "@lucia-auth/adapter-prisma";
+import { sveltekit } from 'lucia-auth/middleware'
 import lucia from 'lucia-auth';
+import { dev } from '$app/environment'
 
 
 export const auth = lucia({
-  adapter: prismaAdapter(prisma),
-  env: "DEV"
+  adapter: prismaAdapter(prisma as any),
+  env: dev ? 'DEV' : 'PROD',
+  middleware: sveltekit(),
+  transformDatabaseUser: (userData) => {
+		return {
+			userId: userData.id,
+			username: userData.username,
+			name: userData.name
+		}
+	}
 });
+
+export type Auth = typeof auth
